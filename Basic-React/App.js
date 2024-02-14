3a1d557ad591ffb50983e7112d41d2512dbac765
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-02-14 22:32:41 +0530

    created nested div using React

--- Basic-React/App.js
+++ Basic-React/App.js
@@ -1,10 +1,28 @@
-const heading = React.createElement(
-    "h1", 
-    {id : "heading", xyz : "abc"}, 
-    "Hello world from React!");
+//nested div
 
-    console.log(heading);
+const parent = React.createElement(
+    "div",
+    {id : "parent"},
+    React.createElement(
+        "div",
+        {id: "child"},
+        React.createElement(
+            "h1",
+            {},
+            "I am a h1 tag"
+        )
+    )
+);
+
+// const heading = React.createElement(
+//     "h1", 
+//     {id : "heading", xyz : "abc"}, 
+//     "Hello world from React!");
+
+    // console.log(heading); //object
+
+    console.log(parent);
 
     const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    root.render(heading);
+    root.render(parent);
