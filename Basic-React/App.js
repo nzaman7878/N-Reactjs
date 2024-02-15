c19223278aef00e002ffaf493b8c92cc4a53b377
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-02-15 21:45:31 +0530

    added functional heading component

--- Basic-React/App.js
+++ Basic-React/App.js
@@ -16,19 +16,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is Namaste React 🚀"),
-    React.createElement("h2", {}, "by NURUZ Zaman"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "This is Namaste React 🚀"),
+//     React.createElement("h2", {}, "by NURUZ Zaman"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I'm an h1 tag"),
+//     React.createElement("h2", {}, "I'm an h2 tag"),
+//   ]),
+// ]);
 
-console.log(parent); // object
+// console.log(parent); // object
+
+const Title = () => (
+    <h1 className="head" tabIndex="5">Namaste React using JSX</h1>
+); // React element
+
+// React Fragment - behave like an empty tag
+const HeadingComponent = () => (
+    <>
+    <div id="container">
+        <Title />
+        <h1 className="heading">React Functional Component</h1>
+    </div>
+
+    <div id="conatainer-2"></div>
+    </>
+);
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
