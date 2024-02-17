24c45b2b20088b01607cd16dec92344eaa52b1f1
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-02-17 23:50:51 +0530

    useEffect hook and call swiggy api

--- Swiggy-Clone/src/components/Body.js
+++ Swiggy-Clone/src/components/Body.js
@@ -1,10 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+
+
 import React from "react";
 const Body = () => {
   // Local State Variable - Super powerful variable
-  const [listOfRestaurants, setListOfRestraunt] = useState(resList);
+  const [listOfRestaurants, setListOfRestraunt] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  useEffect(()=> {
+    fetchData();
+ }, []);
+
+ const fetchData = async ()=> {
+  const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.1745742&lng=91.76177799999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+ 
+  
+ const json = await data.json();
+
+ //optional chaining
+
+ setListOfRestraunt(json?.data?.cards[2]?.data?.data?.cards);
+ setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+
+} ;
 
   return (
     <div className="body">
@@ -22,7 +40,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
