b26410b4ea7c23972d71d2961067670c576da526
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-02-18 00:19:14 +0530

    added search button

--- Swiggy-Clone/src/components/Body.js
+++ Swiggy-Clone/src/components/Body.js
@@ -1,33 +1,64 @@
+import React from "react";
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
-import React from "react";
+
 const Body = () => {
   // Local State Variable - Super powerful variable
   const [listOfRestaurants, setListOfRestraunt] = useState([]);
   const [filteredRestaurant, setFilteredRestaurant] = useState([]);
-  useEffect(()=> {
+
+  const [searchText, setSearchText] = useState("");
+
+  // Whenever state variables update, react triggers a reconciliation cycle(re-renders the component)
+  console.log("Body Rendered");
+
+  useEffect(() => {
     fetchData();
- }, []);
+  }, []);
 
- const fetchData = async ()=> {
-  const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.1745742&lng=91.76177799999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
- 
-  
- const json = await data.json();
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.1745742&lng=91.76177799999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
 
- //optional chaining
+    const json = await data.json();
 
- setListOfRestraunt(json?.data?.cards[2]?.data?.data?.cards);
- setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    // Optional Chaining
+    setListOfRestraunt(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+  };
 
-} ;
-
-   return listOfRestaurants.length === 0 ? (
+  return listOfRestaurants.length === 0 ? (
     <Shimmer />
   ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              // Filter the restraunt cards and update the UI
+              // searchText
+              console.log(searchText);
+
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.data.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
