08b24ecad3040a8c6e3acb7a7dbe6191b898697e
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-02-21 22:17:22 +0530

    tailwind css added in header

--- Swiggy-Clone/src/components/Header.js
+++ Swiggy-Clone/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
 import { Link } from "react-router-dom";
@@ -10,26 +11,26 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
 
   return (
-    <div className="">
+    <div className="flex justify-between bg-pink-100 shadow-lg sm:bg-yellow-50 lg:bg-green-50">
       <div className="logo-container">
-        <img className="" src={LOGO_URL} />
+        <img className="w-56" src={LOGO_URL} />
       </div>
-      <div className="">
-        <ul className="">
-          <li className="">Online Status: {onlineStatus ? "✅" : "🔴"}</li>
-          <li className="">
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-4">Online Status: {onlineStatus ? "✅" : "🔴"}</li>
+          <li className="px-4">
             <Link to="/">Home</Link>
           </li>
-          <li className="">
+          <li className="px-4">
             <Link to="/about">About Us</Link>
           </li>
-          <li className="">
+          <li className="px-4">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li className="">
+          <li className="px-4">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li className="">Cart</li>
+          <li className="px-4">Cart</li>
           <button
             className="login"
             onClick={() => {
